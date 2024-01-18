32e54be721533b9afb2c795694d1c905067aef27
gyeongza <gyeongza@naver.com>
2024-01-18 16:20:11 +0900

    feat: Header 컴포넌트 구현

---
+++ src/components/common/Header.tsx
@@ -0,0 +1,33 @@
+import { css } from '@emotion/react';
+import HeaderLogo from '../../assets/HeaderLogo';
+import { colors } from '../../styles/colorPalette';
+import Flex from './Flex';
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <Flex align="center" css={headerContainer}>
+      <Link to="/" css={logoContainer}>
+        <HeaderLogo />
+      </Link>
+    </Flex>
+  );
+}
+
+const headerContainer = css`
+  position: sticky;
+  top: 0;
+  max-width: 600px;
+  height: 57px;
+  margin: 0 auto;
+  background-color: ${colors.black};
+  z-index: 10;
+  box-shadow: 0 5px 5px 0 ${colors.grey};
+`;
+
+const logoContainer = css`
+  padding: 12px;
+  cursor: pointer;
+`;
+
+export default Header;
